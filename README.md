cd02777baa6ace36b811e5d0584cf7bb59008a47
atmosmps <atmos.souza@luizalabs.com>
2022-06-06 23:09:19 -0300

    chore: add languages setup

--- README.md
+++ README.md
@@ -2,11 +2,5 @@
 ```
 #install_python
 #install_pyenv and config envs like today
-
-#install_golang
-#install_rustlang
-#install_lisplang
-#install_haskkelllang
-
 # add setp to clean linux pos installation
 ```
--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-installation_files_folder="downloaded-installation-files"
+installationFilesFolder="DownloadedInstallationFilesFolder"
 
 start() {
-  mkdir ${installation_files_folder}
-  echo -e "# Installation File Folder\n.${installation_files_folder}/" >> .gitignore
+  mkdir ${installationFilesFolder}
+  echo -e "# Installation Files Folder\n.${installationFilesFolder}/" >> .gitignore
 
   sudo apt update
 
@@ -58,9 +58,9 @@ install_source_code_pro_font() {
     echo "Found fonts dir ${fontsDir}"
   fi
 
-  wget -qO https://fonts.google.com/download?family=Source%20Code%20Pro ${installation_files_folder}
-  unzip ${installation_files_folder}/Source_Code_Pro.zip -d source_code_pro_fonts
-  # mv ${installation_files_folder/source_code_pro_fonts/static/*.ttf fontsDir # esta linha é necessaria, entender porque não está funcionando
+  wget -qO https://fonts.google.com/download?family=Source%20Code%20Pro ${installationFilesFolder}
+  unzip ${installationFilesFolder}/Source_Code_Pro.zip -d source_code_pro_fonts
+  # mv ${installationFilesFolder/source_code_pro_fonts/static/*.ttf fontsDir # esta linha é necessaria, entender porque não está funcionando
   echo "fc-cache -f -v"
   fc-cache -f -v
 }
@@ -74,11 +74,44 @@ install_java() {
   sudo apt-get install openjdk-11-jdk -y
 }
 
-install_nvm() {
+install_nvm_node() {
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | zsh
   export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
   source ~/.zshrc
+  nvm install 11
+  nvm use 11
+}
+
+install_golang() {
+  wget https://dl.google.com/go/go1.18.3.linux-amd64.tar.gz ${installationFilesFolder}
+  sudo tar -C /usr/local/ -xzf go1.18.3.linux-amd64.tar.gz
+  {
+    echo \
+    "#ConfigGolang-----------------------"
+    export PATH=$PATH:/usr/local/go/bin
+    export GOPATH=$HOME/Workspace
+    "#ConfigGolangEnd--------------------"
+  } >> ~/.zshrc
+  source ~/.zshrc
+  go version
+}
+
+install_rust_lang() {
+  # https://github.com/rust-lang/rustup/issues/297
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -sSf | sh -s -- -y
+  rustc --version
+}
+
+install_lisp_lang() {
+  sudo apt-get install sbcl -y
+  # Install Quicklisp package manager
+  curl -o /tmp/ql.lisp http://beta.quicklisp.org/quicklisp.lisp
+  sbcl --no-sysinit --no-userinit --load /tmp/ql.lisp \
+       --eval '(quicklisp-quickstart:install :path "~/.quicklisp")' \
+       --eval '(ql:add-to-init-file)' \
+       --quit
+
 }
 
 install_docker_and_docker_compose() {
@@ -173,8 +206,8 @@ install_spotify() {
 }
 
 install_stremio() {
-  curl -O https://dl.strem.io/shell-linux/v4.4.142/stremio_4.4.142-1_amd64.deb ${installation_files_folder}
-  sudo dpkg -i ${installation_files_folder}/stremio_4.4.142-1_amd64.deb
+  curl -O https://dl.strem.io/shell-linux/v4.4.142/stremio_4.4.142-1_amd64.deb ${installationFilesFolder}
+  sudo dpkg -i ${installationFilesFolder}/stremio_4.4.142-1_amd64.deb
   sudo apt-get install -f -y
 }
 
@@ -194,8 +227,8 @@ install_vlc() {
 }
 
 install_wps_office() {
-  curl -O https://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/linux/10976/wps-office_11.1.0.10976.XA_amd64.deb ${installation_files_folder}
-  sudo dpkg -i ${installation_files_folder}/wps-office_11.1.0.10976.XA_amd64.deb
+  curl -O https://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/linux/10976/wps-office_11.1.0.10976.XA_amd64.deb ${installationFilesFolder}
+  sudo dpkg -i ${installationFilesFolder}/wps-office_11.1.0.10976.XA_amd64.deb
   sudo apt-get install -f -y
 }
 
@@ -204,8 +237,8 @@ install_xmind() {
 }
 
 install_google_chrome() {
-    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb ${installation_files_folder}
-    sudo dpkg -i ${installation_files_folder}/google-chrome-stable_current_amd64.deb
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb ${installationFilesFolder}
+    sudo dpkg -i ${installationFilesFolder}/google-chrome-stable_current_amd64.deb
     sudo apt-get install -f -y
 }
 
@@ -223,8 +256,8 @@ install_intellij() {
 
 install_php_storm() {
   sudo mkdir /opt/jetbrains/
-  curl -O https://download-cdn.jetbrains.com/webide/PhpStorm-2022.1.2.tar.gz ${installation_files_folder}
-  sudo mv ${installation_files_folder}/PhpStorm-20*.tar.gz /opt/jetbrains/
+  curl -O https://download-cdn.jetbrains.com/webide/PhpStorm-2022.1.2.tar.gz ${installationFilesFolder}
+  sudo mv ${installationFilesFolder}/PhpStorm-20*.tar.gz /opt/jetbrains/
   sudo tar zxvf /opt/jetbrains/PhpStorm-20*.tar.gz
   # adicionar step para criação do arquivo.desktop
 }
