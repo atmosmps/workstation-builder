35b727c2ddb0683616d7fb8aafde88d14127f905
atmosmps <atmos.souza@luizalabs.com>
2022-06-07 09:16:35 -0300

    chore: add cleaning function

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -266,8 +266,32 @@ install_postman() {
   sudo snap install postman
 }
 
+cleaning() {
+  sudo apt-get autoremove -y
+
+  # Remove installationFilesFolder
+  rm -rf ${installationFilesFolder}
+
+  # Clean up APT cache in Ubuntu
+  sudo du -sh /var/cache/apt
+  sudo apt-get clean -y
+
+  # Clear systemd journal logs [Intermediate knowledge]
+  journalctl --disk-usage
+  sudo journalctl --vacuum-time=3d
+
+  # Remove older versions of Snap applications
+  du -h /var/lib/snapd/snaps
+  sh ./remove-old-versions-of-snap.sh
+
+  # Clean the thumbnail cache
+  du -sh ~/.cache/thumbnails
+  rm -rf ~/.cache/thumbnails/*
+}
+
 setup() {
   start
+  cleaning
 }
 
 setup
