4d03aac0b26a4dabc2023d9f2e1011fb7bc94844
atmosmps <atmos.souza@luizalabs.com>
2022-06-18 16:22:49 -0300

    chore: end setup builds for ubuntu

--- build.sh
+++ build.sh
@@ -1,8 +1,57 @@
 #!/bin/bash
 
-# Identifies which OS was selected by the user and calls the correct build file
-# You have selected OS XPTO to be configured.
-# You confirm the workstation build for this OS?
-# 0 - Yes, proceed to construction
-# 1 - No, I will choose another OS
-  # Set only one OS option in the file: setup.sh
+source scripts/log.sh
+source functions/text.sh
+source functions/packages.sh
+source functions/files.sh
+
+# Responsible for checking if the configuration file exists.
+setup_exists
+
+source setup.sh
+
+date_start=$(date +'%Y-%m-%d %T')
+
+source globalScripts/hello.sh
+
+if [ "${packages[ubuntu]}" = true ]
+then
+    question_echo "You will create the workstation in the Ubuntu distro.\n"
+    question_echo "Do you want continue? (1) Yes (0) No"
+    read -r continue
+
+    if [ "$continue" = 0 ]
+    then
+        exclamation_echo "OK...process interrupted."
+        exit
+    fi
+
+    source ubuntu/build.sh
+fi
+
+if [ "${packages[popOS]}" = true ]
+then
+    exclamation_echo "This script does not yet support popOS distro. :("
+    exclamation_echo "See the README for how to contribute to this."
+    exit
+fi
+
+if [ "${packages[arch]}" = true ]
+then
+    exclamation_echo "This script does not yet support Arch distro. :("
+    exclamation_echo "See the README for how to contribute to this."
+    exit
+fi
+
+if [ "${packages[nix]}" = true ]
+then
+    exclamation_echo "This script does not yet support NIX distro. :("
+    exclamation_echo "See the README for how to contribute to this."
+    exit
+fi
+
+# Installing checking installed packages
+source globalScripts/checking_installed_packages.sh
+
+# process completion
+source globalScripts/finalization.sh
--- ubuntu/build.sh
+++ ubuntu/build.sh
@@ -5,16 +5,6 @@ source functions/text.sh
 source functions/packages.sh
 source functions/files.sh
 
-# Responsible for checking if the configuration file exists.
-setup_exists
-
-source setup.sh
-
-date_start=$(date +'%Y-%m-%d %T')
-
-# Installing
-source ../globalScripts/hello.sh
-
 # Essentials packages
 source scripts/essentials_packages.sh
 
@@ -45,9 +35,3 @@ fi
 
 # Post installation cleaning
 source scripts/cleaning.sh
-
-# Installing checking installed packages
-source ../globalScripts/checking_installed_packages.sh
-
-# process completion
-source ../globalScripts/finalization.sh
