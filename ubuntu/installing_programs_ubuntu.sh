c8d183997c951d115a1ec0979565a71a293adf40
atmosmps <atmos.souza@luizalabs.com>
2022-06-18 15:28:45 -0300

    chore: add setup desktip apps installations

--- ubuntu/installing_programs_ubuntu.sh
+++ ubuntu/installing_programs_ubuntu.sh
@@ -1,54 +1,5 @@
 #!/bin/bash
 
-install_pyenv() {
-  return 0
-}
-
-install_anki() {
-  sudo snap install anki-ppd
-}
-
-install_dbeaver() {
-  sudo add-apt-repository ppa:serge-rider/dbeaver-ce
-  sudo apt-get update
-  sudo apt-get install dbeaver-ce -y
-}
-
-install_insync() {
-  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ACCAF35C
-  # If the previous command did not work, use this instead:
-  # sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys ACCAF35C
-
-  sudo echo "deb http://apt.insync.io/ubuntu jammy non-free contrib" >> /etc/apt/sources.list.d/insync.list
-  sudo apt-get update
-  sudo apt-get install insync -y
-}
-
-install_peek() {
-  sudo add-apt-repository ppa:peek-developers/stable
-  sudo apt update
-  sudo apt install peek -y
-}
-
-install_qbittorrent() {
-  sudo apt-get update && sudo apt-get install qbittorrent -y
-}
-
-install_remmina() {
-  sudo apt-add-repository ppa:remmina-ppa-team/remmina-next
-  sudo apt update
-  sudo apt install remmina remmina-plugin-rdp remmina-plugin-secret -y
-  sudo killall remmina
-}
-
-install_slack() {
-  sudo snap install slack
-}
-
-install_spotify() {
-  sudo snap install spotify
-}
-
 install_stremio() {
   curl -O https://dl.strem.io/shell-linux/v4.4.142/stremio_4.4.142-1_amd64.deb ${installationFilesFolder}
   sudo dpkg -i ${installationFilesFolder}/stremio_4.4.142-1_amd64.deb
@@ -62,58 +13,14 @@ install_sublime_text() {
   sudo apt-get install sublime-text -y
 }
 
-install_telegram() {
-  sudo snap install telegram-desktop
-}
-
-install_vlc() {
-  sudo apt-get install vlc -y
-}
-
 install_wps_office() {
   curl -O https://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/linux/10976/wps-office_11.1.0.10976.XA_amd64.deb ${installationFilesFolder}
   sudo dpkg -i ${installationFilesFolder}/wps-office_11.1.0.10976.XA_amd64.deb
   sudo apt-get install -f -y
 }
 
-install_xmind() {
-  sudo snap install xmind
-}
-
 install_google_chrome() {
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb ${installationFilesFolder}
     sudo dpkg -i ${installationFilesFolder}/google-chrome-stable_current_amd64.deb
     sudo apt-get install -f -y
-}
-
-install_firefox() {
-  sudo apt-get install firefox -y
-}
-
-install_vs_code() {
-  sudo snap install code --classic
-}
-
-install_intellij() {
-  sudo snap install intellij-idea-community --classic
-}
-
-install_clion() {
-  sudo snap install clion --classic
-}
-
-install_php_storm() {
-  sudo mkdir /opt/jetbrains/
-  curl -O https://download-cdn.jetbrains.com/webide/PhpStorm-2022.1.2.tar.gz ${installationFilesFolder}
-  sudo mv ${installationFilesFolder}/PhpStorm-20*.tar.gz /opt/jetbrains/
-  sudo tar zxvf /opt/jetbrains/PhpStorm-20*.tar.gz
-  # adicionar step para criação do arquivo.desktop
-}
-
-install_postman() {
-  sudo snap install postman
-}
-
-install_resp_app() {
-  sudo snap install redis-desktop-manager
 }
--- ubuntu/scripts/desktop_tools.sh
+++ ubuntu/scripts/desktop_tools.sh
@@ -4,18 +4,44 @@ title_echo "INSTALLING TOOLS"
 
 update_packages
 
-if [ "${packages[mysql-workbench]}" = true ]
+if [ "${packages[anki]}" = true ]
 then
-    process_install_echo "mysql-workbench" "MySQL Workbench"
+    install_echo "Starting installation: Anki"
+
+    if [ "${packages[snapd]}" != true ]
+    then
+        line_echo "Anki installation requires the snapd package..."
+        line_echo "Enable Snapd to true in setup.sh file and try again."
+    elif [ "${packages[snapd]}" = true ]
+    then
+        line_echo "Installing Anki with Snap"
+        sudo snap install anki-ppd 1> /dev/null 2> /dev/stdout
+    fi
+
+    success_install_echo "Finished installation: Anki"
 fi
 
 if [ "${packages[dbeaver]}" = true ]
 then
     install_echo "Starting installation: DBeaver"
 
-    wget -c -P ./temp https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb 1> /dev/null 2> /dev/stdout
-    sudo dpkg -i /tmp/dbeaver-ce_latest_amd64.deb 1> /dev/null 2> /dev/stdout
-    rm /tmp/dbeaver-ce_latest_amd64.deb 1> /dev/null 2> /dev/stdout
+    if [ "${packages[snapd]}" = true ]
+    then
+        line_echo "Installing Dbeaver with Snap"
+        sudo snap install dbeaver-ce 1> /dev/null 2> /dev/stdout
+    elif [ "${packages[snapd]}" != true ]
+    then
+        line_echo "Installing Dbeaver with .deb"
+
+        sudo add-apt-repository ppa:serge-rider/dbeaver-ce 1> /dev/null 2> /dev/stdout
+        update_packages
+        sudo apt-get install dbeaver-ce 1> /dev/null 2> /dev/stdout
+
+        #wget -c -P /tmp/ https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb 1> /dev/null 2> /dev/stdout
+        #sudo dpkg -i /tmp/dbeaver-ce_latest_amd64.deb 1> /dev/null 2> /dev/stdout
+        #sudo apt -f install 1> /dev/null 2> /dev/stdout
+        #sudo rm /tmp/dbeaver-ce_latest_amd64.deb 1> /dev/null 2> /dev/stdout
+    fi
 
     success_install_echo "Finished installation: DBeaver"
 fi
@@ -24,34 +50,96 @@ if [ "${packages[slack]}" = true ]
 then
     install_echo "Starting installation: Slack"
 
-    wget -c -P ./temp https://downloads.slack-edge.com/linux_releases/slack-desktop-4.12.0-amd64.deb
-    sudo apt install ./temp https://downloads.slack-edge.com/linux_releases/slack-desktop-4.12.0-amd64.deb 1> /dev/null 2> /dev/stdout
-    rm /tmp/slack-desktop-4.12.0-amd64.deb 1> /dev/null 2> /dev/stdout
+    if [ "${packages[snapd]}" = true ]
+    then
+        line_echo "Installing Slack with Snap"
+        sudo snap install slack 1> /dev/null 2> /dev/stdout
+    elif [ "${packages[snapd]}" != true ]
+    then
+        line_echo "Installing Slack with .deb"
 
-    success_install_echo "Finished installation: Skype"
+        wget -c -P /tmp/ https://downloads.slack-edge.com/releases/linux/4.26.1/prod/x64/slack-desktop-4.26.1-amd64.deb 1> /dev/null 2> /dev/stdout
+        sudo dpkg -i /tmp/slack-desktop-4.26.1-amd64.deb 1> /dev/null 2> /dev/stdout
+        sudo apt -f install 1> /dev/null 2> /dev/stdout
+        sudo rm /tmp/slack-desktop-4.26.1-amd64.deb 1> /dev/null 2> /dev/stdout
+    fi
+
+    success_install_echo "Finished installation: Slack"
 fi
 
-if [ "${packages[sublime-text]}" = true ]
-then
-    wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add - 1> /dev/null 2> /dev/stdout
-    sudo apt install apt-transport-https 1> /dev/null 2> /dev/stdout
-    echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list 1> /dev/null 2> /dev/stdout
-    update_packages
-    process_install_echo "sublime-text" "Sublime Text 3"
-fi
-
-if [ "${packages[code]}" = true ]
+if [ "${packages[vscode]}" = true ]
 then
     install_echo "Starting installation: VSCode"
 
-    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg 1> /dev/null 2> /dev/stdout
-    sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/ 1> /dev/null 2> /dev/stdout
-    sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list' 1> /dev/null 2> /dev/stdout
-    sudo apt install apt-transport-https 1> /dev/null 2> /dev/stdout
-    sudo apt update 1> /dev/null 2> /dev/stdout
-    sudo apt install code 1> /dev/null 2> /dev/stdout
+    if [ "${packages[snapd]}" = true ]
+    then
+        line_echo "Installing VSCode with Snap"
+        sudo snap install code --classic 1> /dev/null 2> /dev/stdout
+    elif [ "${packages[snapd]}" != true ]
+    then
+        line_echo "Installing VSCode with .deb"
+
+        sudo apt-get install gpg 1> /dev/null 2> /dev/stdout
+        wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+        sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/ 1> /dev/null 2> /dev/stdout
+        sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list' 1> /dev/null 2> /dev/stdout
+        rm -f packages.microsoft.gpg 1> /dev/null 2> /dev/stdout
+        sudo apt update 1> /dev/null 2> /dev/stdout
+        sudo apt install code 1> /dev/null 2> /dev/stdout
+    fi
 
     success_install_echo "Finished installation: VSCode"
+fi
+
+if [ "${packages[intellij]}" = true ]
+then
+    install_echo "Starting installation: Intellij"
+
+    if [ "${packages[snapd]}" != true ]
+    then
+        line_echo "Intellij installation requires the snapd package..."
+        line_echo "Enable Snapd to true in setup.sh file and try again."
+    elif [ "${packages[snapd]}" = true ]
+    then
+        line_echo "Installing Intellij with Snap"
+        sudo snap install intellij-idea-community --classic 1> /dev/null 2> /dev/stdout
+    fi
+
+    success_install_echo "Finished installation: Intellij"
+fi
+
+if [ "${packages[clion]}" = true ]
+then
+    install_echo "Starting installation: Clion"
+
+    if [ "${packages[snapd]}" != true ]
+    then
+        line_echo "Clion installation requires the snapd package..."
+        line_echo "Enable Snapd to true in setup.sh file and try again."
+    elif [ "${packages[snapd]}" = true ]
+    then
+        line_echo "Installing Clion with Snap"
+        sudo snap install clion --classic 1> /dev/null 2> /dev/stdout
+    fi
+
+    success_install_echo "Finished installation: Clion"
+fi
+
+if [ "${packages[php-storm]}" = true ]
+then
+    install_echo "Starting installation: PHPStorm"
+
+    if [ "${packages[snapd]}" != true ]
+    then
+        line_echo "PHPStorm installation requires the snapd package..."
+        line_echo "Enable Snapd to true in setup.sh file and try again."
+    elif [ "${packages[snapd]}" = true ]
+    then
+        line_echo "Installing PHPStorm with Snap"
+        sudo snap install phpstorm --classic 1> /dev/null 2> /dev/stdout
+    fi
+
+    success_install_echo "Finished installation: PHPStorm"
 fi
 
 if [ "${packages[remmina]}" = true ]
@@ -61,13 +149,159 @@ then
     process_install_echo "remmina" "Remmina"
     process_install_echo "remmina-plugin-rdp" "Remmina Plugin RDP"
     process_install_echo "remmina-plugin-secret" "Remmina Plugin Secret"
+    sudo killall remmina
 fi
 
-if [ "${packages[telegram-desktop]}" = true ]
+if [ "${packages[postman]}" = true ]
 then
-    sudo add-apt-repository ppa:atareao/telegram 1> /dev/null 2> /dev/stdout
+    install_echo "Starting installation: Postman"
+
+    if [ "${packages[snapd]}" != true ]
+    then
+        line_echo "Postman installation requires the snapd package..."
+        line_echo "Enable Snapd to true in setup.sh file and try again."
+    elif [ "${packages[snapd]}" = true ]
+    then
+        line_echo "Installing Postman with Snap"
+        sudo snap install postman 1> /dev/null 2> /dev/stdout
+    fi
+
+    success_install_echo "Finished installation: Postman"
+fi
+
+if [ "${packages[insync]}" = true ]
+then
+    install_echo "Starting installation: Insync"
+
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ACCAF35C 1> /dev/null 2> /dev/stdout
+    # If the previous command did not work, use this instead:
+    # sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys ACCAF35C
+
+    sudo echo "deb http://apt.insync.io/ubuntu jammy non-free contrib" >> /etc/apt/sources.list.d/insync.list
     update_packages
-    process_install_echo "telegram" "Telegram Desktop"
+    process_install_echo "insync" "Insync"
+
+    success_install_echo "Finished installation: Insync"
+fi
+
+if [ "${packages[peek]}" = true ]
+then
+    install_echo "Starting installation: Peek"
+
+    sudo add-apt-repository ppa:peek-developers/stable
+    update_packages
+    process_install_echo "peek" "Peek"
+
+    success_install_echo "Finished installation: Peek"
+fi
+
+if [ "${packages[qbittorrent]}" = true ]
+then
+    install_echo "Starting installation: QBittorrent"
+
+    update_packages
+    process_install_echo "qbittorrent" "QBittorrent"
+
+    success_install_echo "Finished installation: QBittorrent"
+fi
+
+if [ "${packages[spotify]}" = true ]
+then
+    install_echo "Starting installation: Spotify"
+
+    if [ "${packages[snapd]}" != true ]
+    then
+        line_echo "Spotify installation requires the snapd package..."
+        line_echo "Enable Snapd to true in setup.sh file and try again."
+    elif [ "${packages[snapd]}" = true ]
+    then
+        line_echo "Installing Spotify with Snap"
+        sudo snap install spotify 1> /dev/null 2> /dev/stdout
+    fi
+
+    success_install_echo "Finished installation: Spotify"
+fi
+
+if [ "${packages[telegram]}" = true ]
+then
+    install_echo "Starting installation: Telegram"
+
+    if [ "${packages[snapd]}" != true ]
+    then
+        line_echo "Telegram installation requires the snapd package..."
+        line_echo "Enable Snapd to true in setup.sh file and try again."
+    elif [ "${packages[snapd]}" = true ]
+    then
+        line_echo "Installing Telegram with Snap"
+        sudo snap install telegram-desktop 1> /dev/null 2> /dev/stdout
+    fi
+
+    success_install_echo "Finished installation: Telegram"
+fi
+
+if [ "${packages[xmind]}" = true ]
+then
+    install_echo "Starting installation: Xmind"
+
+    if [ "${packages[snapd]}" != true ]
+    then
+        line_echo "Xmind installation requires the snapd package..."
+        line_echo "Enable Snapd to true in setup.sh file and try again."
+    elif [ "${packages[snapd]}" = true ]
+    then
+        line_echo "Installing Xmind with Snap"
+        sudo snap install xmind 1> /dev/null 2> /dev/stdout
+    fi
+
+    success_install_echo "Finished installation: Xmind"
+fi
+
+if [ "${packages[resp-app]}" = true ]
+then
+    install_echo "Starting installation: Resp App"
+
+    if [ "${packages[snapd]}" != true ]
+    then
+        line_echo "Resp App installation requires the snapd package..."
+        line_echo "Enable Snapd to true in setup.sh file and try again."
+    elif [ "${packages[snapd]}" = true ]
+    then
+        line_echo "Installing Resp App with Snap"
+        sudo snap install redis-desktop-manager 1> /dev/null 2> /dev/stdout
+    fi
+
+    success_install_echo "Finished installation: Resp App"
+fi
+
+if [ "${packages[vlc]}" = true ]
+then
+    install_echo "Starting installation: VLC"
+
+    update_packages
+    process_install_echo "vlc" "VLC"
+
+    success_install_echo "Finished installation: VLC"
+fi
+
+if [ "${packages[firefox]}" = true ]
+then
+    install_echo "Starting installation: Firefox"
+
+    update_packages
+    process_install_echo "firefox" "Firefox"
+
+    success_install_echo "Finished installation: Firefox"
+fi
+
+# ------------------
+
+if [ "${packages[sublime-text]}" = true ]
+then
+    wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add - 1> /dev/null 2> /dev/stdout
+    sudo apt install apt-transport-https 1> /dev/null 2> /dev/stdout
+    echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list 1> /dev/null 2> /dev/stdout
+    update_packages
+    process_install_echo "sublime-text" "Sublime Text 3"
 fi
 
 echo ""
